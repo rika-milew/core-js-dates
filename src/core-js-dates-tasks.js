40ad9f8c2114ad97ee35119635a9984b4d420355
Erika Milevskaya <rika.gumerova@gmail.com>
2025-12-05 16:00:24 +0300

    feat: implement getNextFriday function

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -76,8 +76,17 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const currentDate = new Date(date);
+  const currentDay = currentDate.getDay();
+
+  let plusDays = (5 - currentDay + 7) % 7;
+  if (plusDays === 0) {
+    plusDays = 7;
+  }
+  const nextFriday = new Date(currentDate);
+  nextFriday.setDate(currentDate.getDate() + plusDays);
+  return nextFriday;
 }
 
 /**
